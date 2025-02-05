8056c98035fd1349de97180f663336bfb9f2add3
Yuvraj <yuvraj280605@gmail.com>
2025-02-05 13:26:21 +0530

    Intial Commit

---
+++ src/Easy/fib.java
@@ -0,0 +1,19 @@
+package Easy;
+//Problem-509
+class Solution2 {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
+        int f=0;
+        int s=1;
+        for (i=1; i<=n; i++){
+            int t=f+s;
+            f=s;
+            s=f;
+        }
+        return f;
+    }
+}
