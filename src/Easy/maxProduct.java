e839ab4200d3c67f129cbec07929de96191e2e61
Yuvraj <yuvraj280605@gmail.com>
2025-02-09 10:27:41 +0530

    Initial commit

---
+++ src/Easy/maxProduct.java
@@ -0,0 +1,19 @@
+package Easy;
+//Problem-1464
+public class maxProduct {
+    public int maxProduct(int[] nums) {
+        int smax=-1;
+        int max=-1;
+
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                smax=max;
+                max=nums[i];
+            }else if(smax<nums[i]){
+                smax=nums[i];
+            }
+        }
+        int ans=(max-1)*(smax-1);
+        return ans;
+    }
+}
