48482ef9794c572e47d27f62ffe781ef8f2a144b
Yuvraj <yuvraj280605@gmail.com>
2025-02-12 12:30:11 +0530

    Initial commit

---
+++ src/Easy/missingNumber.java
@@ -0,0 +1,14 @@
+package Easy;
+//Problem-268
+public class missingNumber {
+    public int missingNumber(int[] nums) {
+        int range=nums.length;
+        int actualSum=(range*(range+1))/2;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++){
+            currSum=currSum+nums[i];
+        }
+        int ans=actualSum-currSum;
+        return ans;
+    }
+}
