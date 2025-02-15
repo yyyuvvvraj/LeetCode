6346f67205143998a85ef17c9e351d9a7d0abb67
Yuvraj <yuvraj280605@gmail.com>
2025-02-15 18:27:08 +0530

    Initial commit

---
+++ src/Medium/twoSum_II.java
@@ -0,0 +1,22 @@
+package Medium;
+//Problem-167
+public class twoSum_II {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans=new int[2];
+        int start=0;
+        int end=numbers.length-1;
+        while(start<end){
+            int sum=numbers[start]+numbers[end];
+            if(sum==target){
+                ans[0]=start+1;
+                ans[1]=end+1;
+                return ans;
+            }else if(sum>target){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return ans;
+    }
+}
