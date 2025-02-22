e86aa1abfacc7e78f72efe5b98f1f7eea5ba008f
Yuvraj <yuvraj280605@gmail.com>
2025-02-22 18:58:59 +0530

    Initial commit

---
+++ src/Easy/findLucky.java
@@ -0,0 +1,25 @@
+package Easy;
+
+import java.util.HashMap;
+
+//Problem-1394
+public class findLucky {
+    public int findLucky(int[] arr) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int ele=arr[i];
+            if(hm.containsKey(ele)==true){
+                hm.put(ele,hm.get(ele)+1);
+            }else{
+                hm.put(ele,1);
+            }
+        }
+        int ans=-1;
+        for(int key:hm.keySet()){
+            if(hm.get(key)==key){
+                ans=Math.max(ans,key);
+            }
+        }
+        return ans;
+    }
+}
