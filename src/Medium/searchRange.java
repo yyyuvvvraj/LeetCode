d23ef47208632aab6b8baafd07e635020316ecc5
Yuvraj <yuvraj280605@gmail.com>
2025-02-24 14:29:53 +0530

    Initial commit

--- src/Medium/searchRange.java
+++ src/Medium/searchRange.java
@@ -12,7 +12,7 @@ public class searchRange {
     }
     public int firstOccurence(int[] nums,int target){
         int start=0;
-        int end=nums.length;
+        int end=nums.length-1;
         int ans=-1;
         while(start<=end){
             int mid=(start+end)/2;
@@ -29,7 +29,7 @@ public class searchRange {
     }
     public int lastOccurence(int[] nums,int target){
         int start=0;
-        int end=nums.length;
+        int end=nums.length-1;
         int ans=-1;
         while(start<=end){
             int mid=(start+end)/2;
