a4bdf0df220d004e0cc69d79c4409bf1a9f2a454
Yuvraj <yuvraj280605@gmail.com>
2025-03-03 11:09:54 +0530

    Initial commit

---
+++ src/DailyChallenges/pivotArray.java
@@ -0,0 +1,24 @@
+package DailyChallenges;
+//Problem-2161
+public class pivotArray {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int[] ans=new int[nums.length];
+        int i=0;
+        for(final int num:nums){
+            if(num<pivot){
+                ans[i++]=num;
+            }
+        }
+        for(final int num:nums){
+            if(num==pivot){
+                ans[i++]=num;
+            }
+        }
+        for(final int num:nums){
+            if(num>pivot){
+                ans[i++]=num;
+            }
+        }
+        return ans;
+    }
+}
