5a11437eae5fd03b0c6d836520fa1d275d634f5f
Yuvraj <yuvraj280605@gmail.com>
2025-03-12 14:24:35 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/maximumCount.java
@@ -0,0 +1,37 @@
+package DailyChallenges.Easy;
+//Problem-2529
+public class maximumCount {
+    public int maximumCount(int[] nums) {
+        int lastnegIndex = lastNegative(nums) + 1;  // Count of negative numbers
+        int firposIndex = nums.length - firstPositive(nums);  // Count of positive numbers
+        return Math.max(lastnegIndex, firposIndex);
+    }
+
+    private int lastNegative(int[] nums) {
+        int start = 0, end = nums.length - 1, ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < 0) {  // Only consider negative numbers
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private int firstPositive(int[] nums) {
+        int start = 0, end = nums.length - 1, ans = nums.length;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > 0) {  // Only consider positive numbers
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/Easy/MyStack.java
@@ -0,0 +1,54 @@
+package Easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static com.sun.tools.javac.main.Option.X;
+
+//Problem-225
+public class MyStack {
+    private Queue<Integer> main;
+    private Queue<Integer> helper;
+    public MyStack() {
+        main=new LinkedList<>();
+        helper=new LinkedList<>();
+    }
+
+    public void push(int x) {
+        //moving elements from main to helper
+        while (main.size()>0){
+            helper.add(main.remove());
+        }
+        //add X in main queue
+        main.add(X);
+        //moving all elements from helper to main
+        while(helper.size()>0){
+            main.add(helper.remove());
+        }
+
+    }
+
+    public int pop() {
+        return main.remove();
+    }
+
+    public int top() {
+        return main.peek();
+    }
+
+    public boolean empty() {
+        if(main.size()==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
