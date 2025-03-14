81f4dfde448ce4582ef8026551b1aec6365d6949
Yuvraj <yuvraj280605@gmail.com>
2025-03-14 09:43:34 +0530

    Initial commit

---
+++ src/DailyChallenges/Medium/maximumCandies.java
@@ -0,0 +1,23 @@
+package DailyChallenges.Medium;
+
+import java.util.Arrays;
+
+//Problem-2226
+public class maximumCandies {
+    public int maximumCandies(int[] candies, long k) {
+        int l=1;
+        int r=(int) (Arrays.stream(candies).asLongStream().sum()/k);
+        while(l<r){
+            int m=(l+r)/2;
+            if(numChildren(candies,m)<k){
+                r=m;
+            }else{
+                l=m+1;
+            }
+        }
+        return numChildren(candies,l)>=k?l:l-1;
+    }
+    private long numChildren(int[] candies,int m){
+        return Arrays.stream(candies).asLongStream().reduce(0L,(subtotal, c)-> subtotal+ c/m);
+    }
+}
