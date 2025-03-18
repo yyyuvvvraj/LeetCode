ae064a1d812a004e1be11236e7f158e32b417e9e
Yuvraj <yuvraj280605@gmail.com>
2025-03-18 15:34:59 +0530

    Initial commit

---
+++ src/DailyChallenges/Medium/longestNiceSubarrays.java
@@ -0,0 +1,17 @@
+package DailyChallenges.Medium;
+//Problem-2401
+public class longestNiceSubarrays {
+    public int longestNiceSubarray(int[] nums) {
+        int ans=0;
+        int usedBits=0;
+
+        for(int l=0,r=0;r<nums.length;++r){
+            while((usedBits & nums[r])>0){
+                usedBits ^=nums[l++];
+            }
+            usedBits |=nums[r];
+            ans=Math.max(ans,r-l+1);
+        }
+        return ans;
+    }
+}
