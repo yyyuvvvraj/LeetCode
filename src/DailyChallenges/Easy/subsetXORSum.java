83d819bb4f80ef2ba00ca5641153c626a8a56933
Yuvraj <yuvraj280605@gmail.com>
2025-04-05 10:03:27 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/subsetXORSum.java
@@ -0,0 +1,15 @@
+package DailyChallenges.Easy;
+//Problem-1863
+public class subsetXORSum {
+    public int subsetXORSum(int[] nums) {
+        return dfs(nums,0,0);
+    }
+    private int dfs(int[] nums,int i,int xors){
+        if(i==nums.length){
+            return xors;
+        }
+        final int x=dfs(nums,i+1,xors);
+        final int y=dfs(nums,i+1,nums[i]^xors);
+        return x+y;
+    }
+}
