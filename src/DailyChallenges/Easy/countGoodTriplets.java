06a2753b18d52f0428271ef66ceb764d9008da86
Yuvraj <yuvraj280605@gmail.com>
2025-04-14 10:43:15 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/countGoodTriplets.java
@@ -0,0 +1,18 @@
+package DailyChallenges.Easy;
+//Problem-1534
+public class countGoodTriplets {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n=arr.length;
+        int cnt=0;
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                for(int k=j+1;k<n;++k){
+                    if(Math.abs(arr[i]-arr[j])<=a&&Math.abs(arr[j]-arr[k])<=b&&Math.abs(arr[i]-arr[k])<=c){
+                        ++cnt;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+}
