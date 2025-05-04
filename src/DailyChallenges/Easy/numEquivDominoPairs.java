9009dcd24190d6eab05649b8f46b8f26e2153314
Yuvraj <yuvraj280605@gmail.com>
2025-05-04 10:27:03 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/numEquivDominoPairs.java
@@ -0,0 +1,20 @@
+package DailyChallenges.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Problem-1128
+public class numEquivDominoPairs {
+    class Solution {
+        public int numEquivDominoPairs(int[][] dominoes) {
+            int ans=0;
+            Map<Integer,Integer> count=new HashMap<>();
+            for(int[] domino:dominoes){
+                int key=Math.min(domino[0],domino[1])*10+Math.max(domino[0],domino[1]);
+                ans+=count.getOrDefault(key,0);
+                count.merge(key,1,Integer::sum);
+            }
+            return ans;
+        }
+    }
+}
