f2326fef252583fcc2c367581cc8ffea9f604a84
Yuvraj <yuvraj280605@gmail.com>
2025-05-06 15:09:50 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/buildArray.java
@@ -0,0 +1,14 @@
+package DailyChallenges.Easy;
+//Problem-1920
+public class buildArray {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;++i){
+            nums[i]+=n*(nums[nums[i]]%n);
+        }
+        for(int i=0;i<n;++i){
+            nums[i]/=n;
+        }
+        return nums;
+    }
+}
