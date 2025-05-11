da216cadb9ec5e93a91727205471b7091a112311
Yuvraj <yuvraj280605@gmail.com>
2025-05-11 15:05:31 +0530

    Initial commit

---
+++ src/DailyChallenges/Easy/threeConsecutiveOdds.java
@@ -0,0 +1,12 @@
+package DailyChallenges.Easy;
+//Problem-1550
+public class threeConsecutiveOdds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i]%2==1&&arr[i+1]%2==1&&arr[i+2]%2==1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
