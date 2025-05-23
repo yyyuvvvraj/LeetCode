6452c9b1e71d1f74e0b51695a77ca2163efcdd66
Yuvraj <yuvraj280605@gmail.com>
2025-05-23 18:59:00 +0530

    Initial commit

---
+++ src/DailyChallenges/Hard/maximumValueSum.java
@@ -0,0 +1,18 @@
+package DailyChallenges.Hard;
+//Problem-3068
+public class maximumValueSum {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long maxSum=0;
+        int changedCount=0;
+        int minChangeDiff=Integer.MAX_VALUE;
+        for(final int num:nums){
+            maxSum+=Math.max(num,num^k);
+            changedCount+=((num^k)>num)?1:0;
+            minChangeDiff=Math.min(minChangeDiff,Math.abs(num-(num^k)));
+        }
+        if(changedCount%2==0){
+            return maxSum;
+        }
+        return maxSum-minChangeDiff;
+    }
+}
