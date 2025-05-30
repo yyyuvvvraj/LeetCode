1777e36ae6d6a990e5e4ed09e89552c80973c784
Yuvraj <yuvraj280605@gmail.com>
2025-05-30 14:44:20 +0530

    Initial commit

--- src/DailyChallenges/Medium/closestMeetingNode.java
+++ src/DailyChallenges/Medium/closestMeetingNode.java
@@ -3,37 +3,36 @@ import java.util.Arrays;
 
 //Problem-2359
 public class closestMeetingNode {
-
     class Solution {
         public int closestMeetingNode(int[] edges, int node1, int node2) {
-            int kMax = 10000;
             int[] dist1 = getDist(edges, node1);
             int[] dist2 = getDist(edges, node2);
-            int minDist = kMax;
+            int minDist = Integer.MAX_VALUE;
             int ans = -1;
 
             for (int i = 0; i < edges.length; ++i) {
-                if (Math.min(dist1[i], dist2[i]) >= 0) {
+                if (dist1[i] >= 0 && dist2[i] >= 0) {
                     int maxDist = Math.max(dist1[i], dist2[i]);
                     if (maxDist < minDist) {
                         minDist = maxDist;
-                        ans = i; // Corrected this line
+                        ans = i;
                     }
                 }
             }
             return ans;
         }
 
-        private int[] getDist(int[] edges, int u) {
+        private int[] getDist(int[] edges, int start) {
             int[] dist = new int[edges.length];
             Arrays.fill(dist, -1);
             int d = 0;
-            while (u != -1 && dist[u] == -1) {
-                dist[u] = d++;
-                u = edges[u];
+            while (start != -1 && dist[start] == -1) {
+                dist[start] = d++;
+                start = edges[start];
             }
             return dist;
         }
     }
+
 
 }
